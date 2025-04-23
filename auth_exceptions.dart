0afb4175fecf90aceb4ad7f6132af2a5033aaff2
kelvin coder <65468321+coddotcom@users.noreply.github.com>
2025-04-23 13:01:42 +0300

    Create auth_exceptions.dart

---
+++ auth_exceptions.dart
@@ -0,0 +1,15 @@
+class UserNotFoundException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+//register exceptions
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+//generic exception
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
